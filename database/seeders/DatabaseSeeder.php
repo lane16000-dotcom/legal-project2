7ba4fec4a5f72386871c69ac8d6e7fd1162f5a16
lane16000-dotcom <lane16000@gmail.com>
2026-06-17 12:12:31 +0300

    الرفع

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        // 2. زراعة الأدوار الخمسة الرسمية بمسمياتها الصحيحة والمعرّفات المرتبة
+        // 2. زراعة الأدوار  الرسمية بمسمياتها الصحيحة والمعرّفات المرتبة
         DB::table('roles')->insert([
             ['role_id' => 1, 'role_name' => 'manager', 'created_at' => now(), 'updated_at' => now()],
             ['role_id' => 2, 'role_name' => 'employee', 'created_at' => now(), 'updated_at' => now()],
